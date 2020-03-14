54cfa99530f93f0801565882c2dfadc43410b98e
priya4321 <48145641+priya4321@users.noreply.github.com>
2020-03-14 12:18:24 +0530

    Add files via upload
    project file test is attached

--- Test.js
+++ Test.js
@@ -2,7 +2,7 @@ describe('Projec', function() {
     it('Login Screen', function() {
       cy.clearCookies()
      
-     cy.visit('stgclient.vetty.co/client/login', { timeout: 100000 })
+     cy.visit('stgclient.vetty.co/client/login', { timeout: 60000 })
       
       cy.get('input[name=email]').should('be.visible').should('be.enabled').type('vijay@tweeny.in')
       cy.get('input[name=passowrd]').should('be.visible').should('be.enabled').type('123456')
@@ -16,10 +16,7 @@ describe('Projec', function() {
       cy.get('#firstname').should('be.enabled').type('Priya')
       cy.get('#lastname').should('be.enabled').type('Kharatmol')
       cy.get('#email').should('be.enabled').type('Priyatest@gmail.com')
-      cy.get('.greyarrow').click()
-      cy.select('BENGALORE').should('have.value','BENGALORE')
-      cy.wait(1000)
-
+      cy.get('select')
       cy.select('Testt').should('have.value','11')
       cy.wait(1000)
       cy.get('. mx-auto btn-blue lg place-order').click()
